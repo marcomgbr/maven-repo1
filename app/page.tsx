9e5142adbf9af0d037bd4d680ddf85ed1a957fca
marco <marco.mg.br@gmail.com>
2024-09-07 14:54:32 -0300

    add dashboard remaining items

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,6 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            //href="/login"
             href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
